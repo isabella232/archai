21db98373bb406510b9ae58dd336de44acf81a2e
Debadeepta Dey <dedey@microsoft.com>
2022-02-26 19:44:19 -0800

    Note the metrics code for datasets with less than 5 classes is still not robust.

--- archai/common/metrics.py
+++ archai/common/metrics.py
@@ -104,11 +104,10 @@ class Metrics:
         # update metrics after optimizer step
         batch_size = x.size(0)
 
-        #TODO: Provide a better fix for cases when we have less than 5 classes
-        if y.shape[1] < 5:
-            top1, top5 = ml_utils.accuracy(logits, y, topk=(1, 1))
-        else:
-            top1, top5 = ml_utils.accuracy(logits, y, topk=(1, 5))
+        # NOTE: This is going to be a silent bug when we have a 
+        # dataset which actually has >5 classes
+        top1, top5 = ml_utils.accuracy(logits, y, topk=(1, 1))
+        #top1, top5 = ml_utils.accuracy(logits, y, topk=(1, 5))
 
         epoch = self.run_metrics.cur_epoch()
         epoch.post_step(top1.item(), top5.item(),
